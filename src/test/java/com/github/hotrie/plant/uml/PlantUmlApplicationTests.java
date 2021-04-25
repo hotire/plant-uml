bba0fb80ae5fccce606d9d8df7645e1e56abd701
hotire <gngh0101@gmail.com>
2021-04-26 01:16:01 +0900

    init

---
+++ src/test/java/com/github/hotrie/plant/uml/PlantUmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hotrie.plant.uml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantUmlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
